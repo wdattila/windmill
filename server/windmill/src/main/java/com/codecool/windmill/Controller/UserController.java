2acb784e357cffae9b37d2397d5063a0aa3582c8
wdattila <wdattila@gmail.com>
2022-08-23 15:56:50 +0200

    Add: post user endpoint

--- server/windmill/src/main/java/com/codecool/windmill/Controller/UserController.java
+++ server/windmill/src/main/java/com/codecool/windmill/Controller/UserController.java
@@ -5,9 +5,12 @@ import com.codecool.windmill.Model.User;
 import com.codecool.windmill.Service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/user")
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -18,5 +21,11 @@ public class UserController {
     public UserDTO getUser(@PathVariable(name = "id") Long id){
         User user = userService.getById(id);
         return modelMapper.map(user, UserDTO.class);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<?> postUser(@RequestBody User user){
+        userService.addUser(user);
+        return ResponseEntity.ok(modelMapper.map(user, UserDTO.class));
     }
 }

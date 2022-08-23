166f1debe7d17d61beb868644027b0fb8c2a8a4d
wdattila <wdattila@gmail.com>
2022-08-23 13:55:17 +0200

    Add: get user in controller

--- server/windmill/src/main/java/com/codecool/windmill/Controller/UserController.java
+++ server/windmill/src/main/java/com/codecool/windmill/Controller/UserController.java
@@ -1,11 +1,22 @@
 package com.codecool.windmill.Controller;
 
+import com.codecool.windmill.DTO.UserDTO;
+import com.codecool.windmill.Model.User;
 import com.codecool.windmill.Service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("/api/user")
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping("/get/{id}")
+    public UserDTO getUser(@PathVariable(name = "id") Long id){
+        User user = userService.getById(id);
+        return modelMapper.map(user, UserDTO.class);
+    }
 }

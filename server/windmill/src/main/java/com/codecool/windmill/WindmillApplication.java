c5630a3f66b308b91e4b8d27b4a90bdfdecaeba8
wdattila <wdattila@gmail.com>
2022-08-23 10:04:58 +0200

    Add: Spring framework

---
+++ server/windmill/src/main/java/com/codecool/windmill/WindmillApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.windmill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindmillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindmillApplication.class, args);
+    }
+
+}
---
+++ server/windmill/src/test/java/com/codecool/windmill/WindmillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.windmill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindmillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

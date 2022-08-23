c217ea4e291953ca91ffa6ca8127487b93051f89
wdattila <wdattila@gmail.com>
2022-08-23 13:55:06 +0200

    Add: get user in service

--- server/windmill/src/main/java/com/codecool/windmill/Service/UserService.java
+++ server/windmill/src/main/java/com/codecool/windmill/Service/UserService.java
@@ -1,7 +1,16 @@
 package com.codecool.windmill.Service;
 
+import com.codecool.windmill.Model.User;
+import com.codecool.windmill.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getById(Long id){
+        return userRepository.findById(id).orElse(null);
+    }
 }

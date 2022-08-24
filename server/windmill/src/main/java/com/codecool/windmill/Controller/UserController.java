834aff5b5b4f32e9b4437272dad2c1b21da138ad
wdattila <wdattila@gmail.com>
2022-08-24 09:22:24 +0200

    Add: login entrypoint

--- server/windmill/src/main/java/com/codecool/windmill/Controller/UserController.java
+++ server/windmill/src/main/java/com/codecool/windmill/Controller/UserController.java
@@ -3,9 +3,16 @@ package com.codecool.windmill.Controller;
 import com.codecool.windmill.DTO.UserDTO;
 import com.codecool.windmill.Model.User;
 import com.codecool.windmill.Service.UserService;
+import com.codecool.windmill.Util.JwtTokenUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
@@ -16,6 +23,12 @@ public class UserController {
     private UserService userService;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/get/{id}")
     public UserDTO getUser(@PathVariable(name = "id") Long id){
@@ -25,7 +38,27 @@ public class UserController {
 
     @PostMapping("/post")
     public ResponseEntity<?> postUser(@RequestBody User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.addUser(user);
         return ResponseEntity.ok(modelMapper.map(user, UserDTO.class));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) throws Exception {
+        Authentication auth = new UsernamePasswordAuthenticationToken(user.getName(), user.getPassword());
+        auth = authenticate(auth);
+
+        String token = jwtTokenUtil.generateToken(user.getName());
+        return ResponseEntity.ok(token);
+    }
+
+    private Authentication authenticate(Authentication authentication) throws Exception {
+        try {
+            return authenticationManager.authenticate(authentication);
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 }

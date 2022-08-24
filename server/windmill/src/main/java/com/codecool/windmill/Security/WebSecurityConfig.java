e4c40fbb43f4689fb7c16c4bca44edd92b91791e
wdattila <wdattila@gmail.com>
2022-08-24 09:21:34 +0200

    Add: Authentication manager bean, jwt to security

--- server/windmill/src/main/java/com/codecool/windmill/Security/WebSecurityConfig.java
+++ server/windmill/src/main/java/com/codecool/windmill/Security/WebSecurityConfig.java
@@ -1,9 +1,17 @@
 package com.codecool.windmill.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,6 +19,24 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+    @Autowired
+    private JwtAuthenticationEntrypoint jwtAuthenticationEntrypoint;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder())
+                .and().build();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -18,6 +44,12 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests((authz) -> authz
                         .anyRequest().permitAll()
                 )
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntrypoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .httpBasic(withDefaults());
         return http.build();
     }

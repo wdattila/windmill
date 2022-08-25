6e571c25f56d2172ea86d6be3162bef261ac7822
wdattila <wdattila@gmail.com>
2022-08-25 10:10:40 +0200

    Add: react router link to index, login

--- client/windmill/src/Components/Navbar.js
+++ client/windmill/src/Components/Navbar.js
@@ -1,22 +1,22 @@
 import CSSModules from "react-css-modules"
+import { Link } from "react-router-dom"
 import styles from "./Navbar.module.css"
 
 const Navbar = () => {
-    return <div className="app">
-  <aside styleName="sidebar">
-         <header>
-        Menu
-      </header>
-    <nav styleName="sidebar-nav">
- 
-      <ul>
-        <li>
-          <a href="#"><i className="fa-solid fa-right-to-bracket"></i> <span>Login</span></a>
-        </li>
-      </ul>
-    </nav>
-  </aside>
-</div>   
+  return <>
+    <aside styleName="sidebar">
+      <header>Menu</header>
+      <nav styleName="sidebar-nav">
+        <ul>
+          <li>
+            <Link to='/' className="link"><i class="fa-solid fa-house"></i> <span>Home</span></Link>
+          </li>
+          <li>
+            <Link to='/login' className="link"><i className="fa-solid fa-right-to-bracket"></i> <span>Login</span></Link>
+          </li>
+        </ul>
+      </nav>
+    </aside></>
 }
 
 export default CSSModules(Navbar, styles, {allowMultiple: true})

2d236dbaa485882ca349ea3dd1f4748f0e981e50
wdattila <wdattila@gmail.com>
2022-08-25 09:54:10 +0200

    Add: useAuth hook

--- client/windmill/src/Context/AuthProvider.js
+++ client/windmill/src/Context/AuthProvider.js
@@ -1,13 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { login } from "../Api/UserApi";
 
 let AuthContext = createContext(null);
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+}
 
 let AuthProvider = ({children}) => {
     const [accessToken, setAccessToken] = useState(localStorage.getItem('access_token'));
 
     const signIn = async (username, password) => {
-        let token = await login(password, username);
+        let token = await login(username, password);
         if(token){
             setAccessToken(token);
             localStorage.setItem('access_token', token);
@@ -20,3 +24,5 @@ let AuthProvider = ({children}) => {
         {children}
     </AuthContext.Provider>
 }
+
+export default AuthProvider;

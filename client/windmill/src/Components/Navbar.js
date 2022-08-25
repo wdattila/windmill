df5429a3d3594535efafc22cc6197183280d7ec2
wdattila <wdattila@gmail.com>
2022-08-25 10:29:01 +0200

    Add: logout option

--- client/windmill/src/Components/Navbar.js
+++ client/windmill/src/Components/Navbar.js
@@ -1,19 +1,28 @@
 import CSSModules from "react-css-modules"
 import { Link } from "react-router-dom"
+import { useAuth } from "../Context/AuthProvider"
 import styles from "./Navbar.module.css"
 
 const Navbar = () => {
+  const auth = useAuth();
+
   return <>
     <aside styleName="sidebar">
       <header>Menu</header>
       <nav styleName="sidebar-nav">
         <ul>
           <li>
-            <Link to='/' className="link"><i class="fa-solid fa-house"></i> <span>Home</span></Link>
+            <Link to='/' className="link"><i className="fa-solid fa-house"></i> <span>Home</span></Link>
           </li>
-          <li>
-            <Link to='/login' className="link"><i className="fa-solid fa-right-to-bracket"></i> <span>Login</span></Link>
-          </li>
+          {auth.accessToken?
+            <li>
+              <Link to='/logout' className="link"><i className="fa-solid fa-right-from-bracket"></i> <span>Logout</span></Link>
+            </li>
+            :
+            <li>
+              <Link to='/login' className="link"><i className="fa-solid fa-right-to-bracket"></i> <span>Login</span></Link>
+            </li>
+          }
         </ul>
       </nav>
     </aside></>

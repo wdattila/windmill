3fd9771275235ab1f2f7ede85a8a8bb1ec744ddd
wdattila <wdattila@gmail.com>
2022-08-25 09:56:06 +0200

    Add: components to app

--- client/windmill/src/App.js
+++ client/windmill/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import AuthProvider from './Context/AuthProvider';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RequiredAuth from './Context/RequiredAuth';
+import LoginForm from './Components/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<RequiredAuth/>}>
+              
+            </Route>
+            <Route path='/login' element={<LoginForm />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
